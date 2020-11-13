8e6c8473ee9c1f33396f1d55ba8e627cc5851b33
sungaosheng <sungaosheng@xizi.com>
2020-11-13 16:21:00 +0800

    feat: add step

--- src/components/RandomAvatar/RandomAvatar.tsx
+++ src/components/RandomAvatar/RandomAvatar.tsx
@@ -8,22 +8,27 @@ interface IAvatar {
   headimg: string;
 }
 
-// random avatar
+let interval: NodeJS.Timeout;
+
+// Random avatar
 function RandomAvatar() {
-  // mock data
+  // Mock data
   const [avatarList, setAvatarList] = useState<Array<IAvatar>>([]);
 
-  // componentDidMount
+  // ComponentDidMount
   useEffect(() => {
     getFackApi();
+    return () => clearInterval(interval);
   }, []);
 
-  /** get mock data */
+  /** Get mock data */
   const getFackApi = async () => {
     const data = await mockAvatar;
 
     // step 1：Add random attributes to the avatar
     const randomAvatarList = addAvatarAttributes(data);
+    // step 2: Divide into a two-dimensional array
+    divideInToTDArray(randomAvatarList);
   };
 
   /**
@@ -32,15 +37,15 @@ function RandomAvatar() {
    */
   const addAvatarAttributes = (arr: Array<IAvatar>) => {
     return arr.map((item) => {
-      // box random width height
+      // Box random width height
       let whRandom = getRandomInt(100);
-      // min width height 60px
+      // Min width height 60px
       whRandom = Math.round(100 * (whRandom < 60 ? 60 : whRandom));
-      // box random position
+      // Box random position
       let positionRandom = getRandomInt(50);
-      // animation delay time
+      // Animation delay time
       let delayRandom = parseFloat((Math.random() * Math.floor(1)).toFixed(1));
-      // return new avatar object
+      // Return new avatar object
       return {
         ...item,
         width: whRandom,
@@ -50,6 +55,61 @@ function RandomAvatar() {
         aniDelay: delayRandom,
       };
     });
+  };
+
+  /**
+   * Divide into a two-dimensional array
+   * @param {Array<IAvatar>} arr
+   * @param {number} ODLength Length of one-dimensional array
+   */
+  const divideInToTDArray = (arr: Array<IAvatar>, ODLength: number = 12) => {
+    let TDArray: Array<Array<IAvatar>> = [];
+
+    // If the avatar length is less than the one-dimensional length
+    if (arr.length < ODLength) {
+      TDArray = [arr];
+    } else {
+      // Put into a new array according to the one-dimensional length
+      const length = Math.round(arr.length / ODLength);
+
+      for (let i = 0; i < length; i++) {
+        TDArray.push(arr.slice(i * ODLength, (i + 1) * ODLength));
+      }
+    }
+
+    // The index of the array selected to be played
+    let playIndex = 0;
+
+    // step 3: Timed loop array
+    interval = setInterval(() => {
+      setAvatarList([]);
+
+      if (playIndex < TDArray.length) {
+        randomPushArr(TDArray[playIndex], ODLength);
+        playIndex++;
+      } else {
+        playIndex = 0;
+        randomPushArr(TDArray[playIndex], ODLength);
+      }
+    }, 4000);
+  };
+
+  /**
+   * Random push into one-dimensional
+   * @param {Array<IAvatar>} arr
+   * @param {number} ODLength Length of one-dimensional array
+   */
+  const randomPushArr = (arr: Array<IAvatar>, ODLength: number) => {
+    let ODArray = Array(ODLength).fill("");
+
+    arr.forEach((item) => {
+      // Ignore duplicates
+      const index = getRandomInt(ODLength);
+      ODArray[index] = item;
+    });
+
+    // step 4: Set avatar into view
+    setAvatarList(ODArray);
   };
 
   return <div>random</div>;
