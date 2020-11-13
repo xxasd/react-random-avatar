b5229e06af062f5a7d07440627160f588500c2a5
sungaosheng <sungaosheng@xizi.com>
2020-11-13 15:22:25 +0800

    fix: change type of typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+// random avatar
+import RandomAvatar from "./components/RandomAvatar/RandomAvatar";
+
+function App() {
+  return (
+    <div className="App">
+      <RandomAvatar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RandomAvatar/RandomAvatar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { mockAvatar } from './mock'
+
+// random avatar
+function RandomAvatar() {
+  return (
+    <div>random</div>
+  )
+}
+
+export default RandomAvatar;
